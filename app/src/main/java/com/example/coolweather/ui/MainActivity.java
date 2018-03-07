ef1a00feea50429434755315a8150a16224ef2b3
liuyang <17603089668@163.com>
2018-03-07 17:37:18 +0800

    完成遍历省市县三级列表的功能

--- app/src/main/java/com/example/coolweather/ui/MainActivity.java
+++ app/src/main/java/com/example/coolweather/ui/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.coolweather.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 import com.example.coolweather.R;
 
@@ -10,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+//        Toast.makeText(this, "进入主页", Toast.LENGTH_SHORT).show();
     }
 }
